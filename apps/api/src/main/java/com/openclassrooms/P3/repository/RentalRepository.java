64041b7fa88c9c67a9dbe4a4223ff48cf558f262
TonyCDalvin <83288223+TonyRDX@users.noreply.github.com>
2025-09-10 14:18:47 +0200

    chore; use JPA repository

--- apps/api/src/main/java/com/openclassrooms/P3/repository/RentalRepository.java
+++ apps/api/src/main/java/com/openclassrooms/P3/repository/RentalRepository.java
@@ -1,11 +1,10 @@
 package com.openclassrooms.P3.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.P3.model.Rental;
 
 @Repository
-public interface RentalRepository extends PagingAndSortingRepository<Rental, Integer>, CrudRepository<Rental, Integer> {
+public interface RentalRepository extends JpaRepository<Rental, Integer>{
 }
